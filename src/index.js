f3f272d3c19231f88730bf668cb6777ba49f8ecc
abhilash15500 <89824888+abhilash15500@users.noreply.github.com>
2024-09-17 18:59:22 +0530

    Implement js logic for adding todos and projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import "./styles.css";
+
+const projects = [{
+    title: "default project",
+    todos: [],
+}];
+
+let currentProject = projects[0].todos;
+
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    pushTodo() {
+        currentProject.push(this);
+    };
+
+    
+
+
+
+}
+
+class Project {
+    constructor(title, todos = []) {
+        this.title = title;
+        this.todos = todos;
+    }
+
+    pushProject() {
+        projects.push(this);
+        currentProject = this.todos;
+    }
+}
+
+
+
+let p1 = new Project("I'm new");
+p1.pushProject();
+
+let t1 = new Todo('title', 'description', 'dueDate', 'priority');
+t1.pushTodo();
+
+console.log(projects);
