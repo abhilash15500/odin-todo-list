5704d748d2b9d7b9ac17fdacf2d5261ce58ec6b9
abhilash15500 <89824888+abhilash15500@users.noreply.github.com>
2024-10-23 12:31:00 +0530

    Make a class constructor for todo and projects

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
 
-console.log(greeting);
+
+const myProjects = [{title:"Default Project",todos : []}];
+
+
+class Todo {
+    constructor(title,description,dueDate,priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+
+}
+
+
+class Project {
+    constructor(title) {
+        this.title = title;
+    }
+    
+}
