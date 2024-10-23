c37b3c78ef486ffb8ae1c6ec986ce11f78a8f064
abhilash15500 <89824888+abhilash15500@users.noreply.github.com>
2024-10-23 21:36:34 +0530

    Implement logic for adding todos to default project by default

--- src/index.js
+++ src/index.js
@@ -1,23 +1,66 @@
 import "./styles.css";
 
-const myProjects = [{
-    title : "Default Project",
-    todos : [],
-}];
+const myProjects = [];
+
+let currentProjectIndex;
 
 class Todo {
-    constructor(title,description,dueDate,priority,completed = false) {
+    constructor(title, description, dueDate, priority, completed = false) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
         this.completed = completed;
     };
-};
 
-class Project {
-    constructor(title,todos = []) {
-        this.title = title;
-        this.todos = todos;
+    markTodoAsComplete() {
+        this.completed = true;
+    };
+
+    changeTodoPriority() {
+        this.priority = priority;
+    };
+
+    pushTodoToArray() {
+        if (currentProjectIndex == undefined) {
+            myProjects[0].todo.push(this);
+        }
+
+        else if (currentProjectIndex) {
+            myProjects[currentProjectIndex].todos.push(this);
+        };
     };
 };
+
+
+class Project {
+    constructor(title, todos = [],uniqueId) {
+        this.title = title;
+        this.todos = todos;
+        this.uniqueId = Math.floor(Math.random() * 10000);
+    };
+
+    pushProjectToArray() {
+        myProjects.push(this);
+
+    };
+
+    currentProjectIdentifier() {
+        let uniqueIdOfProject = this.uniqueId
+        const currentProjectIndex = myProjects.findIndex(project => project.uniqueId == uniqueIdOfProject);
+        console.log(currentProjectIndex);
+    };
+};
+
+
+let defaultProject = new Project("Default Project");
+defaultProject.pushProjectToArray();
+
+
+
+let anotherNewProject = new Project("idk");
+anotherNewProject.pushProjectToArray();
+anotherNewProject.currentProjectIdentifier();
+defaultProject.currentProjectIdentifier();
+console.log(myProjects);
+
